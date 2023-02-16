9a058e07f91253b86a815e4aeb7df129af58b948
Charles <charlesmangin.98@gmail.com>
2023-02-16 17:29:04 +0000

    Formatting

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -4,9 +4,7 @@ import {Link} from "react-router-dom";
 class ProjectItem extends React.Component {
     render() {
         return (
-            <Link className="project-card" key={this.props.name} to={{
-                pathname: `/${this.props.name}`
-            }}>
+            <Link className="project-card" key={this.props.name} to={{pathname: `/${this.props.name}`}}>
                 <img className="project-img" src={this.props.image} alt={this.props.name}/>
 
                 <div className="project-overlay"></div>
@@ -19,7 +17,7 @@ class ProjectItem extends React.Component {
                         {this.props.type}
                     </div>
                 </div>
-                </Link>
+            </Link>
         )
     }
 }
