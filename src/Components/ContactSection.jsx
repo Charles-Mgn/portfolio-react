3718d78a6b6a962aaf52d7bebf6ce8f29f243e86
Charles <charlesmangin.98@gmail.com>
2023-02-16 16:52:02 +0000

    Implementing contact form

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -1,14 +1,37 @@
+import React, { useState } from "react";
+
 const Contact = () => {
+    const [status, setStatus] = useState("Submit");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("Sending...");
+        const { name, email, message } = e.target.elements;
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        });
+        setStatus("Submit");
+        let result = await response.json();
+        alert(result.status);
+    };
     return (
         <section id="contact">
             <h2>Contact</h2>
-            <form action="" className="flex-container">
+            <form onSubmit={handleSubmit} className="flex-container">
                 <div className="flex-container">
-                    <input type="text" className="input-left" placeholder="My name is..."/>
-                    <input type="text" className="input-right" placeholder="And here's my email"/>
+                    <input id="name" type="text" className="input-left" placeholder="My name is..." required/>
+                    <input id="email" type="text" className="input-right" placeholder="And here's my email" required/>
                 </div>
-                <textarea name="" id="" cols="30" rows="10" placeholder="I'm contacting you in order to"></textarea>
-                <input type="submit" value="Envoyer"/>
+                <textarea id="message" cols="30" rows="10" placeholder="I'm contacting you in order to..." required/>
+                <button type="submit">{status}</button>
             </form>
             <div className="social-container flex-container">
                 <a href="https://www.linkedin.com/in/charles-m-9014a4151/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
